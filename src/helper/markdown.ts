1a5e007ecec08a48d08bec7afb6cba1cdb3ec0ac
anpigon <markan82@email.com>
2023-02-18 21:03:44 +0900

    fix: 코드 블럭에 html 태그가 포함된 경우 오류

--- src/helper/markdown.ts
+++ src/helper/markdown.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // ref: https://github.com/markdown-it/markdown-it
 import MarkdownIt from 'markdown-it';
+import { escapeHtml, unescapeAll } from 'markdown-it/lib/common/utils';
 
 const md = new MarkdownIt({
   breaks: true,
@@ -36,21 +37,30 @@ md.set({
   highlight: function (str, lang) {
     // Use your favorite code highlighting library here
     // and return the highlighted code wrapped in a <pre> tag
-    const code = str.replace(/\n/g, '<br>\n').trimEnd();
+    const code = escapeHtml(str).replace(/\n/g, '<br>\n').trimEnd();
     return `<pre class="${lang}" data-ke-language="${lang}"><code>${code}</code></pre>`;
   },
 });
 
 const containerClass = 'tt_article_useless_p_margin contents_style';
 
-md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+md.renderer.rules.fence = function (tokens, idx, options, env, slf) {
   const token = tokens[idx];
-  const lang = token.info.trim();
-  const code = token.content;
-  const content = md?.options?.highlight?.(code, lang, '');
+  const info = token.info ? unescapeAll(token.info).trim() : '';
+  let langName = '';
+  let langAttrs = '';
+
+  if (info) {
+    const arr = info.split(/(\s+)/g);
+    langName = arr[0];
+    langAttrs = arr.slice(2).join('');
+  }
+
+  // escapeHtml
+  const highlighted = md.options.highlight?.(token.content, langName, langAttrs) || escapeHtml(token.content);
 
   // Wrap the highlighted code in a <div> tag
-  return `<div class="${containerClass}">${content}</div>`;
+  return `<div class="${containerClass}"><pre><code ${slf.renderAttrs(token)}>${highlighted}</code></pre></div>\n`;
 };
 
 export function markdownToHtml(markdown: string) {
