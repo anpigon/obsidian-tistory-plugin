981177381fbb77efefdc4bbf33bc2e88ba390f4a
anpigon <markan82@gmail.com>
2023-09-17 10:34:05 +0900

    fix(utils.ts): update setActiveLeaf function call to pass an object with the focus property set to false to prevent the leaf from gaining focus when opening a file

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -3,7 +3,7 @@ import { App, TFile } from 'obsidian';
 export const openFile = (props: { file: TFile; app: App; isNewLeaf: boolean }) => {
   const { file, app, isNewLeaf } = props;
   const leaf = app.workspace.getLeaf(isNewLeaf);
-  app.workspace.setActiveLeaf(leaf, false);
+  app.workspace.setActiveLeaf(leaf, { focus: false });
   leaf.openFile(file, { eState: { focus: true } });
 };
 
