cf325f1d51009ab49746b9e9f2e42a09e25775a7
anpigon <markan82@gmail.com>
2023-12-10 11:00:23 +0900

    feat(utils.ts): add removeFrontmatter function to extract frontmatter from markdown content
    The removeFrontmatter function is added to the utils.ts file. This function takes a markdown content string as input and checks if it contains frontmatter. If frontmatter is found, it extracts the frontmatter string and removes it from the content. The function returns an object with the frontmatter string and the content without the frontmatter. If no frontmatter is found, the function returns an object with empty strings for both properties. This function is useful for processing markdown files that contain frontmatter.

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -10,3 +10,19 @@ export const openFile = (props: { file: TFile; app: App; isNewLeaf: boolean }) =
 export const openFileInNewPane = (app: App, file: TFile) => {
   openFile({ file: file, app: app, isNewLeaf: true });
 };
+
+export const removeFrontmatter = (markdownContent: string) => {
+  const frontmatterRegex = /^---\s*\n([\s\S]*?)\n---\s*\n/;
+  const match = markdownContent.match(frontmatterRegex);
+
+  if (match) {
+    // Extract frontmatter and remove it from the content
+    const frontmatterString = match[0];
+    const contentWithoutFrontmatter = markdownContent.replace(frontmatterRegex, '');
+
+    return { frontmatterString, contentWithoutFrontmatter };
+  } else {
+    // No frontmatter found
+    return { frontmatterString: '', contentWithoutFrontmatter: markdownContent };
+  }
+};
